77ce2d41385c7d08f203231377b640950a18a6d0
AndrewBills <31480930+AndrewBills@users.noreply.github.com>
2017-12-09 23:45:07 -0700

    Add files via upload

--- HouseFunc.cpp
+++ HouseFunc.cpp
@@ -30,11 +30,13 @@ void readCSV(const string fileIn,vector<HousePrice>& hp)
     double price;
     io::CSVReader<7> in(fileIn);
     in.read_header(io::ignore_extra_column, "id", "number", "street", "city","state","postalCode", "price");
+    ///the next line builds the vector array of all of all 1000 houses
     while(in.read_row(id, number, street, city, state, postalCode, price))
     {
         HousePrice temp(id, number, street, city, state, postalCode, price);
         hp.push_back(temp);
     }
+    ///prints out all 1000 houses
     for(int i=0; i<1000; i++)
         cout << hp[i];
     cout << endl;
@@ -48,14 +50,17 @@ void readCSV(const string fileIn,vector<HousePrice>& hp)
  */
 void houseMarketValues(vector<HousePrice> &hp)
 {
+    ///reorders the houses according to price from least to greatest
     for(int i=0;i<1000;i++)
         for (int j=0; j<1000;j++)
 			if(hp[j].getPrice()<hp[i].getPrice())
 				swap(hp[i], hp[j]);
+    ///prints out the most affordable home
     cout << "The most affordable home: ";
     for(int i=0; i<1000; i++)
         if(hp[999].getPrice() == hp[i].getPrice())
 			cout << hp[i];
+    ///prints out the most expansive home
     cout << "The most expansive home: ";
     for(int i=0; i<1000; i++)
 		if(hp[0].getPrice() == hp[i].getPrice())
@@ -75,15 +80,17 @@ void houseMarketPerState(vector<HousePrice> &hp)
                                {"ME"}, {"MI"}, {"MN"}, {"MO"}, {"MS"}, {"MT"}, {"NC"}, {"ND"}, {"NE"}, {"NH"},
                                {"NJ"}, {"NM"}, {"NV"}, {"NY"}, {"OH"}, {"OK"}, {"OR"}, {"PA"}, {"RI"}, {"SC"},
                                {"SD"}, {"TN"}, {"TX"}, {"UT"}, {"VA"}, {"VT"}, {"WA"}, {"WI"}, {"WV"}, {"WY"}};
-
+    ///fills a new vector will all 1000 states
     vector<string> stringarray;
     for (int i = 0; i < 1000; i++)
         stringarray.push_back(hp[i].getState());
-
+    ///sorts the states in alphabetical order
     sort(stringarray.begin(), stringarray.end());
     cout << "Home per state:" << endl;
+    ///counts all the amount of each state and prints them out in matrix form
     for(int i=0, n = 0; i<50; i++)
     {
+        /// this if statement stops any states with 0 houses for sale from showing up on the console
         if(count(stringarray.begin(), stringarray.end(), statesString[i])!=0)
         {
             cout << " " << statesString[i] << " ";
